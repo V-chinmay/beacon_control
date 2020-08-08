d3ce7f0fcbd6e08754cff1db6f83b666539550f0
sirenachin <sirenachin@gmail.com>
2020-08-08 22:17:48 +0530

    experi

--- mm_socket.c
+++ mm_socket.c
@@ -1,10 +1,4 @@
 #include"mm_socket.h"
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<string.h>
-#include <arpa/inet.h>
-
 
 void recv_till_eof(int sock,char* dest)
 {
@@ -21,29 +15,24 @@ void recv_till_eof(int sock,char* dest)
 
 int main()
 {
-    char client_inp[30];
     int opt=1;
-    char server_buff[30]="hello from server";
     int addr_len=sizeof(address_s);
 
     address_s.sin_family=AF_INET;
+    address_s.sin_port=htons(PORT);
+    int sockfd=socket(AF_INET,SOCK_STREAM,0);
 
-    //address_s.sin_addr.s_addr=INADDR_ANY;
-    address_s.sin_port=htons(1500);
-    sockfd=socket(AF_INET,SOCK_STREAM,0);
+    
     if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) 
     { 
         perror("setsockopt"); 
-        exit(EXIT_FAILURE); 
+        return(0);
     } 
-    /*if(inet_pton(AF_INET, "192.168.0.18", &address_s.sin_addr)<0)
-    {
-        printf("invalid address!!\n");
-    }*/
 
     if(bind(sockfd,(struct sockaddr *)&address_s,(socklen_t)addr_len))
     {
         printf("Failed to create socket!!\n");
+        return(0);
     }
     else
     {
@@ -51,25 +40,12 @@ int main()
     }
     
     printf("listening!!\n");
-    listen(sockfd,2);
+ 
+    listen(sockfd,MAX_CON);
 
     int conn_fd=accept(sockfd,(struct sockaddr *)&address_s,(socklen_t*)&addr_len);
-    printf("recived connection!!\n");
-    recv_till_eof(conn_fd,&client_inp[0]);
-    printf("client sent::%s\n",client_inp);
-
-    if(!send(conn_fd,&server_buff,strlen(server_buff),MSG_DONTWAIT))
-    {
-        printf("no data sent!!");
-    }
-    else
-    {
-        printf("successfully sent data to client!!\n");
-    }
-    close(conn_fd);
-    close(sockfd);
+    
+    printf("successfully made the connection!!\n");
+    
     return(1);
-
-
-
 }
